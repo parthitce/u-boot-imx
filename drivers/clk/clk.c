8675d1da9d4cffa58fcec1134a6807382538913f
Lukasz Majewski <lukma@denx.de>
2019-05-17 00:10:41 +0200

    clk: Port Linux common clock framework [CCF] for imx6q to U-boot (tag: 5.0-rc3)
    This commit brings the files from Linux kernel to provide clocks support
    as it is used on the Linux kernel with common clock framework [CCF] setup.
    
    The directory structure has been preserved. The ported code only supports
    reading information from PLL, MUX, Divider, etc and enabling/disabling
    the clocks USDHCx/ECSPIx depending on used bus. Moreover, it is agnostic
    to the alias numbering as the information about the clock is read from
    device tree.
    
    One needs to pay attention to the comments indicating necessary for U-boot's
    DM changes.
    
    If needed the code can be extended to support the "set" part of the clock
    management.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

---
+++ drivers/clk/clk.c
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2019 DENX Software Engineering
+ * Lukasz Majewski, DENX Software Engineering, lukma@denx.de
+ */
+
+#include <common.h>
+#include <clk-uclass.h>
+#include <dm/device.h>
+#include <dm/uclass.h>
+#include <dm/lists.h>
+#include <dm/device-internal.h>
+#include <clk.h>
+
+int clk_register(struct clk *clk, const char *drv_name,
+		 ulong drv_data, const char *name,
+		 const char *parent_name)
+{
+	struct udevice *parent;
+	struct driver *drv;
+	int ret;
+
+	ret = uclass_get_device_by_name(UCLASS_CLK, parent_name, &parent);
+	if (ret)
+		printf("%s: UCLASS parent: 0x%p\n", __func__, parent);
+
+	debug("%s: name: %s parent: %s [0x%p]\n", __func__, name, parent->name,
+	      parent);
+
+	drv = lists_driver_lookup_name(drv_name);
+	if (!drv) {
+		printf("%s: %s is not a valid driver name\n",
+		       __func__, drv_name);
+		return -ENOENT;
+	}
+
+	ret = device_bind_with_driver_data(parent, drv, name, drv_data,
+					   ofnode_null(), &clk->dev);
+	if (ret) {
+		printf("%s: CLK: %s driver bind error [%d]!\n", __func__, name,
+		       ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+ulong clk_generic_get_rate(struct clk *clk)
+{
+	return clk_get_parent_rate(clk);
+}
+
+const char *clk_hw_get_name(const struct clk *hw)
+{
+	return hw->dev->name;
+}
